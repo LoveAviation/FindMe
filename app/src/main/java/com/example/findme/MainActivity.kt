7c584b85682d0515bba983d1ad61825787ae681d
LoveAviation <ivangeorgmarcov@gmail.com>
2024-07-07 11:20:24 +0300

    first commit. bottom bar layour problem

---
+++ app/src/main/java/com/example/findme/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.findme
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.graphics.PorterDuff.Mode;
+import android.view.animation.Animation
+import com.example.findme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private var bot_bar_selected = 1
+    private val KEY = "bot_bar_selected"
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        bot_bar_selected = savedInstanceState.getInt(KEY)
+        chooseBotBar(bot_bar_selected)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.searchButton.setOnClickListener {
+            chooseBotBar(1)
+        }
+        binding.createButton.setOnClickListener {
+            chooseBotBar(2)
+        }
+        binding.accountButton.setOnClickListener {
+            chooseBotBar(3)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(KEY, bot_bar_selected)
+        super.onSaveInstanceState(outState)
+    }
+
+    private fun chooseBotBar(a: Int) {
+        with(binding) {
+            when (a) {
+                1 -> {
+                    searchButton.setColorFilter(getColor(R.color.selected), Mode.SRC_ATOP)
+                    createButton.colorFilter = null
+                    accountButton.colorFilter = null
+                    bot_bar_selected = 1
+                }
+
+                2 -> {
+                    searchButton.colorFilter = null
+                    createButton.setColorFilter(getColor(R.color.selected), Mode.SRC_ATOP)
+                    accountButton.colorFilter = null
+                    bot_bar_selected = 2
+                }
+
+                3 -> {
+                    binding.searchButton.colorFilter = null
+                    binding.createButton.colorFilter = null
+                    binding.accountButton.setColorFilter(getColor(R.color.selected), Mode.SRC_ATOP)
+                    bot_bar_selected = 3
+                }
+            }
+        }
+    }
+}
