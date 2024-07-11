d9f9cbb58bb9b7aaf99600348f5ace96bd3f2f8e
LoveAviation <ivangeorgmarcov@gmail.com>
2024-07-11 22:01:27 +0300

    prework commit

--- app/src/main/java/com/example/findme/CreateFormActivity.kt
+++ app/src/main/java/com/example/findme/CreateFormActivity.kt
@@ -1,14 +1,22 @@
 package com.example.findme
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.findme.databinding.ActivityCreateFormBinding
 
 class CreateFormActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateFormBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_form)
+        binding = ActivityCreateFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backTitleBtn.setOnClickListener {
+            finish()
+        }
     }
 }
